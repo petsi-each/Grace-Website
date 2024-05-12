327a51bc0b29457455fdc9a1185631a9f14ac705
Anemaygi <gianemayumisan@gmail.com>
2024-05-13 00:26:52 +0800

    style: add responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,18 @@
 'use client'
 
 import { usePathname } from "next/navigation";
-import PrimaryButton from "./PrimaryButton";
+import { FiMenu } from "react-icons/fi";
+import { FiX } from "react-icons/fi";
 
-export default function Header(){
-    
+import PrimaryButton from "./PrimaryButton";
+import { useState } from "react";
+
+export default function Header() {
+
+    const [openMenu, setOpenMenu] = useState(false);
+
+    const pathname = usePathname();
+
     const links = [
         {
             title: "Início",
@@ -27,22 +35,26 @@ export default function Header(){
             ref: "http://each.uspnet.usp.br/petsi/grace/?page_id=104"
         }
     ]
-    const pathname = usePathname();
-    
-    return(
-       
-        <header className="text-black md:flex font-Poppins text-base m-12 justify-center items-center">
-            <div>
-                <img src="./logoHorizontal.png" alt="Logo do GRACE" width={125}/>
+
+    return (
+
+        <header className="text-black lg:flex font-Poppins text-base m-12 justify-center items-center bg-brancoGrace">
+
+            <div className="flex items-center lg:justify-center space-x-2">
+                <div className="text-cinzaGrace lg:hidden" onClick={() => setOpenMenu((prevValue) => !prevValue)}>{openMenu ? <FiX size={24} /> : <FiMenu size={24} />}</div>
+                <img src="./logoHorizontal.png" alt="Logo do GRACE" width={125} />
             </div>
-            <nav className="flex md:grow md:flex-row flex-col md:items-center md:justify-center my-2 md:my-0">
+
+            <nav className={`flex md:grow lg:flex-row flex-col lg:items-center lg:justify-center my-2 lg:my-0 ${openMenu ? '' : 'max-lg:hidden'} `}>
                 {
-                    links.map((item) => (<a className={`md:mx-4 md:my-0 xl:mx-8 my-1 hover:font-semibold transition-all ${pathname == item.ref ? 'text-vermelhoGrace font-semibold' : ''}`} href={item.ref} target="_blank" key={item.title}>{item.title}</a>))
+                    links.map((item) => (<a className={`lg:mx-4 lg:my-0 xl:mx-8 my-1 hover:font-semibold transition-all ${pathname == item.ref ? 'text-vermelhoGrace font-semibold' : ''}`} href={item.ref} target="_blank" key={item.title}>{item.title}</a>))
                 }
             </nav>
-            <div>
-                <PrimaryButton onClick={()=>window.open("https://www.instagram.com/graceusp/", "_blank")} title={"Entre em contato"} />
+
+            <div className={`${openMenu ? '' : 'max-lg:hidden'}`}>
+                <PrimaryButton onClick={() => window.open("https://www.instagram.com/graceusp/", "_blank")} title={"Entre em contato"} />
             </div>
+
         </header>
 
     );
