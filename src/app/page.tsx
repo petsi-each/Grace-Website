d9d2d238d33a6be77be32e3cd429f727d18b3964
Anemaygi <gianemayumisan@gmail.com>
2024-05-16 15:58:47 +0800

    chore: use layout.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Header from "@/components/Header";
-
 export default function Home() {
   return (
-    <main className="px-8">
-      <Header/>
+    <main>
+      {/* Adicionar componentes da landingPage aqui aqui */}
     </main>
   );
 }
