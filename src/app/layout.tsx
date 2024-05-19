b7df72b3e96725471ac2fbf6d1d1370c5d0f8b09
Anemaygi <gianemayumisan@gmail.com>
2024-05-19 15:38:25 +0800

    fix: add css to layout page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,14 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
 import Header from "@/components/Header";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "GRACE USP"
+};
 
 export default function RootLayout({
   children,
@@ -6,11 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html>
-      <body className="px-8">
+    <html lang="en">
+      <body className={`px-8 ${inter.className}`}>
 
         <Header />
-
         {children}
 
       </body>
