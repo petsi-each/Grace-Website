933833706f85ca3d816dcc84ca2ddb706222701a
Fernando Toba Lopez <fetobalopez@gmail.com>
2024-05-21 17:32:23 -0300

    Chore: Implement the text of the footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -21,7 +22,7 @@ export default function RootLayout({
 
         <Header />
         {children}
-
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,22 @@
+
+
+export default function Footer(){
+
+    return(
+    <div>
+        <div>
+            <p>
+            oiiii
+            </p>    
+            
+            </div>
+            
+            <div>
+            <p className="text-center font-Poppins text-sm/[12px]">
+                Desenvolvido por PET-SI 2024
+            </p>
+            </div>
+    </div>
+    );
+    
+}  
