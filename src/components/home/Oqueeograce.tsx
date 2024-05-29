6e6bbdc7aa09a3198d8fc1c2e53dc258d10ec8da
segalv <anaclarasvp@usp.br>
2024-05-29 02:46:28 -0300

    Corrigindo adaptabilidade do background ao conteúdo da section

--- src/components/home/Oqueeograce.tsx
+++ src/components/home/Oqueeograce.tsx
@@ -3,9 +3,9 @@ import Balao from "./components/Balao";
 
 export default function Oqueeograce() {
     return (
-        <section className="my-16 grid">
-            <img src="/home/oqueeograceFundo.svg" className="col-start-1 row-start-1 w-full" />
-            <div className="p-40 col-start-1 row-start-1 flex flex-col items-center"> {/*preferi sem justify-center*/}
+        <div className="my-16">
+            <img src="/home/oqueeograceBGT.svg" className="-mb-1 w-screen" /> {/*priorizar uso de w-screen over w-full*/}
+            <section className="bg-gradient-to-b from-vermelhoGrace to-[#feb581] py-2 px-32 flex flex-col items-center"> {/*preferi sem justify-center*/}
                 <h1 className="m-8 font-Poppins font-bold text-brancoGrace text-3xl">O que fazemos?</h1>
                 <p className="font-Poppins text-center text-brancoGrace text-lg">O GRACE é um projeto de extensão do curso de Sistemas de Informação (Escola de Artes, CIências e Humanidades) da Universidade de São Paulo. Nosso objetivo é <b>incentivar</b> e <b>manter</b> meninas na área da computação, a fim de diminuir a disparidade de gênero, por meio de atividades práticas e tecnológicas!</p>
                 <div className="mt-16 grid grid-cols-3 gap-x-5 gap-y-4"> {/*não me preocupei com responsividade mas PRECISAMOS*/}
@@ -28,8 +28,9 @@ export default function Oqueeograce() {
                         <Balao imagem="/home/visitaAsEscolas.jpg" alternativo="Graceanes em uma oficina com crianças do ensino fundamental" titulo="Visita às escolas" descricao="Levando atividades para escolas da região da Zona Leste de SP" />
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+            <img src="/home/oqueeograceBGB.svg" className="w-screen" />
+        </div>
     );
 }
 
