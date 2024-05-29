e09ecc6f018529c38ed09d0d6778ce303402d468
segalv <anaclarasvp@usp.br>
2024-05-29 00:39:47 -0300

    Reajuste de tag para HTML semântico

--- src/components/home/components/Balao.tsx
+++ src/components/home/components/Balao.tsx
@@ -8,10 +8,10 @@ interface Balaopropos{
 
 export default function Balao({ imagem, alternativo, titulo, descricao }:Balaopropos) {
     return (
-        <div className="flex flex-col p-6 w-72 h-72 bg-brancoGrace rounded-3xl shadow-md"> {/*queria ter usado o justify center aqui mas nao deu certo com a imagem! */}
+        <article className="flex flex-col p-6 w-72 h-72 bg-brancoGrace rounded-3xl shadow-md"> {/*queria ter usado o justify center aqui mas nao deu certo com a imagem! */}
             <img className="w-60 h-40 rounded-3xl" src={imagem} alt={alternativo}/>
             <h2 className="font-Poppins font-bold text-vermelhoGrace text-lg flex justify-center m-2">{titulo}</h2>
             <p className="font-Poppins text-center text-black text-xs">{descricao}</p>
-        </div>
+        </article>
     );
 }
