f757b6168fb1532cf53a79d4aa634598320d6d69
segalv <anaclarasvp@usp.br>
2024-05-29 00:26:07 -0300

    Mudança de formato das imagens dos cards

--- src/components/home/Oqueeograce.tsx
+++ src/components/home/Oqueeograce.tsx
@@ -10,22 +10,22 @@ export default function Oqueeograce() {
                 <p className="font-Poppins text-center text-brancoGrace text-lg">O GRACE é um projeto de extensão do curso de Sistemas de Informação (Escola de Artes, CIências e Humanidades) da Universidade de São Paulo. Nosso objetivo é <b>incentivar</b> e <b>manter</b> meninas na área da computação, a fim de diminuir a disparidade de gênero, por meio de atividades práticas e tecnológicas!</p>
                 <div className="mt-16 grid grid-cols-3 gap-x-5 gap-y-4"> {/*não me preocupei com responsividade mas PRECISAMOS*/}
                     <div>
-                        <Balao imagem="/home/oficinasTecnicas.svg" alternativo="Estudantes em computadores" titulo="Oficinas Técnicas" descricao="Para ensino fundamental, médio e graduação" />
+                        <Balao imagem="/home/oficinasTecnicas.jpg" alternativo="Estudantes em computadores" titulo="Oficinas Técnicas" descricao="Para ensino fundamental, médio e graduação" />
                     </div>
                     <div>
-                        <Balao imagem="/home/falaGrace.svg" alternativo="Graceane com um microfone" titulo="Fala, GRACE!" descricao="Podcast em conjunto com PET-SI com mulheres da área" />
+                        <Balao imagem="/home/falaGrace.jpg" alternativo="Graceane com um microfone" titulo="Fala, GRACE!" descricao="Podcast em conjunto com PET-SI com mulheres da área" />
                     </div>
                     <div>
-                        <Balao imagem="/home/eventosDeTecnologia.svg" alternativo="Graceane apresentando projetos do GRACE na semana do curso" titulo="Eventos de Tecnologia" descricao="Ministrando palestras e oficinas abertas" />
+                        <Balao imagem="/home/eventosDeTecnologia.jpg" alternativo="Graceane apresentando projetos do GRACE na semana do curso" titulo="Eventos de Tecnologia" descricao="Ministrando palestras e oficinas abertas" />
                     </div>
                     <div>
-                        <Balao imagem="/home/rodasDeConversa.svg" alternativo="Alunas e professoras reunidas" titulo="Rodas de Conversa" descricao="Com o objetivo de acolher e conversar com as ingressantes" />
+                        <Balao imagem="/home/rodasDeConversa.jpg" alternativo="Alunas e professoras reunidas" titulo="Rodas de Conversa" descricao="Com o objetivo de acolher e conversar com as ingressantes" />
                     </div>
                     <div>
-                        <Balao imagem="/home/escapeRoom.svg" alternativo="Graceanes em frente à sala de escape" titulo="Escape Room" descricao="Desafios sobre ciência e tecnologia para que o grupo desvende" />
+                        <Balao imagem="/home/escapeRoom.jpg" alternativo="Graceanes em frente à sala de escape" titulo="Escape Room" descricao="Desafios sobre ciência e tecnologia para que o grupo desvende" />
                     </div>
                     <div>
-                        <Balao imagem="/home/visitaAsEscolas.svg" alternativo="Graceanes em uma oficina com crianças do ensino fundamental" titulo="Visita às escolas" descricao="Levando atividades para escolas da região da Zona Leste de SP" />
+                        <Balao imagem="/home/visitaAsEscolas.jpg" alternativo="Graceanes em uma oficina com crianças do ensino fundamental" titulo="Visita às escolas" descricao="Levando atividades para escolas da região da Zona Leste de SP" />
                     </div>
                 </div>
             </div>
