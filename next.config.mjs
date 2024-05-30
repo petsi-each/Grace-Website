34c65682d282cb5d7482f00f9bcc07b16cdd5620
Anemaygi <gianemayumisan@gmail.com>
2024-05-30 09:55:13 +0800

    chore: fix basepath depending on env

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  basePath: isProd ? '/Grace-Website' : '',
+  assetPrefix: isProd ? '/Grace-Website/' : '',
+};
 
 export default nextConfig;
