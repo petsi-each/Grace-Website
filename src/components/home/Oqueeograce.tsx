d984f01422ef2f2ca0afe74cd086f7c817619601
segalv <anaclarasvp@usp.br>
2024-05-30 11:10:41 -0300

    Reestruturação da manipulação dos cards no grid; Objetificação das props do componente 'Balao';

--- src/components/home/Oqueeograce.tsx
+++ src/components/home/Oqueeograce.tsx
@@ -2,6 +2,46 @@
 import Balao from "./components/Balao";
 
 export default function Oqueeograce() {
+    const cards = [
+        {
+            imagem:"/home/oficinasTecnicas.jpg",
+            alternativo: "Estudantes em computadores",
+            titulo:"Oficinas Técnicas",
+            descricao:"Para ensino fundamental, médio e graduação"
+        },
+        {
+            imagem:"/home/falaGrace.jpg",
+            alternativo: "Graceane com um microfone",
+            titulo:"Fala, GRACE!",
+            descricao:"Podcast em conjunto com o PET-SI com mulheres na área"
+        },
+        {
+            imagem:"/home/eventosDeTecnologia.jpg",
+            alternativo: "Graceane apresentando projetos do GRACE na semana do curso",
+            titulo:"Eventos de Tecnologia",
+            descricao:"Ministrando palestras e oficinas abertas"
+        },
+        {
+            imagem:"/home/rodasDeConversa.jpg",
+            alternativo: "ALunas e professoras reunidas",
+            titulo:"Rodas de Conversa",
+            descricao:"Com o objetivo de acolher e conversar com as ingressantes"
+        },
+        {
+            imagem:"/home/escapeRoom.jpg",
+            alternativo: "Graceanes em frente à sala de escape",
+            titulo:"Escape Room",
+            descricao:"Desafios sobre ciência e tecnologia para que o grupo desvende"
+        },
+        {
+            imagem:"/home/visitaAsEscolas.jpg",
+            alternativo: "Graceanes em uma oficina com crianças do ensino fundamental",
+            titulo:"Visitando às escolas",
+            descricao:"Levando atividades para escolas da região da Zona Leste de SP"
+        }
+    ]
+
+
     return (
         <div className="my-16">
             <img src="/home/oqueeograceBGT.svg" className="-mb-1 w-screen" /> {/*priorizar uso de w-screen over w-full*/}
@@ -9,24 +49,13 @@ export default function Oqueeograce() {
                 <h1 className="mb-12 font-Poppins font-bold text-brancoGrace text-4xl">O que fazemos?</h1>
                 <p className="font-Poppins text-center text-brancoGrace text-lg">O GRACE é um projeto de extensão do curso de Sistemas de Informação (Escola de Artes, CIências e Humanidades) da Universidade de São Paulo. Nosso objetivo é <b>incentivar</b> e <b>manter</b> meninas na área da computação, a fim de diminuir a disparidade de gênero, por meio de atividades práticas e tecnológicas!</p>
                 <div className="mt-16 grid grid-cols-3 gap-x-5 gap-y-4"> {/*não me preocupei com responsividade mas PRECISAMOS*/}
-                    <div>
-                        <Balao imagem="/home/oficinasTecnicas.jpg" alternativo="Estudantes em computadores" titulo="Oficinas Técnicas" descricao="Para ensino fundamental, médio e graduação" />
-                    </div>
-                    <div>
-                        <Balao imagem="/home/falaGrace.jpg" alternativo="Graceane com um microfone" titulo="Fala, GRACE!" descricao="Podcast em conjunto com PET-SI com mulheres da área" />
-                    </div>
-                    <div>
-                        <Balao imagem="/home/eventosDeTecnologia.jpg" alternativo="Graceane apresentando projetos do GRACE na semana do curso" titulo="Eventos de Tecnologia" descricao="Ministrando palestras e oficinas abertas" />
-                    </div>
-                    <div>
-                        <Balao imagem="/home/rodasDeConversa.jpg" alternativo="Alunas e professoras reunidas" titulo="Rodas de Conversa" descricao="Com o objetivo de acolher e conversar com as ingressantes" />
-                    </div>
-                    <div>
-                        <Balao imagem="/home/escapeRoom.jpg" alternativo="Graceanes em frente à sala de escape" titulo="Escape Room" descricao="Desafios sobre ciência e tecnologia para que o grupo desvende" />
-                    </div>
-                    <div>
-                        <Balao imagem="/home/visitaAsEscolas.jpg" alternativo="Graceanes em uma oficina com crianças do ensino fundamental" titulo="Visita às escolas" descricao="Levando atividades para escolas da região da Zona Leste de SP" />
-                    </div>
+                    {
+                    cards.map((card)=>(
+                        <div>
+                            <Balao imagem={card.imagem} alternativo={card.alternativo} titulo={card.titulo} descricao={card.descricao}/>
+                        </div>
+                    ))
+                    }                    
                 </div>
             </section>
             <img src="/home/oqueeograceBGB.svg" className="w-screen" />
--- src/components/home/components/Balao.tsx
+++ src/components/home/components/Balao.tsx
@@ -1,17 +1,17 @@
 
-interface Balaopropos{
+interface Balaoprops{
     imagem: string,
     alternativo: string,
     titulo: string,
-    descricao: string,
+    descricao: string
 }
 
-export default function Balao({ imagem, alternativo, titulo, descricao }:Balaopropos) {
+export default function Balao(props:Balaoprops) {
     return (
         <article className="flex flex-col p-6 w-72 h-72 bg-brancoGrace rounded-3xl shadow-md"> {/*queria ter usado o justify center aqui mas nao deu certo com a imagem! */}
-            <img className="w-60 h-40 rounded-3xl" src={imagem} alt={alternativo}/>
-            <h2 className="font-Poppins font-bold text-vermelhoGrace text-lg flex justify-center m-2">{titulo}</h2>
-            <p className="font-Poppins text-center text-black text-xs">{descricao}</p>
+            <img className="w-60 h-40 rounded-3xl" src={props.imagem} alt={props.alternativo}/>
+            <h2 className="font-Poppins font-bold text-vermelhoGrace text-lg flex justify-center m-2">{props.titulo}</h2>
+            <p className="font-Poppins text-center text-black text-xs">{props.descricao}</p>
         </article>
     );
 }
