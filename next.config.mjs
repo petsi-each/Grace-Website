a06c30763de1ff7b3a6d3d31f95c8aa63b492170
Anemaygi <gianemayumisan@gmail.com>
2024-05-30 10:29:41 +0800

    chore: test ghactions

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV !== 'production';
+// const isProd = process.env.NODE_ENV !== 'production';
 
 const nextConfig = {
-  basePath: isProd ? '/Grace-Website' : '',
-  assetPrefix: isProd ? '/Grace-Website/' : '',
+  basePath: '/Grace-Website',
+  assetPrefix: '/Grace-Website/',
 };
 
 export default nextConfig;
