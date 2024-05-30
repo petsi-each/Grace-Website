d114c9ade5be38342bd314a91e4c2279275dacd4
Anemaygi <gianemayumisan@gmail.com>
2024-05-30 10:35:57 +0800

    chore: undo next js configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-// const isProd = process.env.NODE_ENV !== 'production';
-
-const nextConfig = {
-  basePath: '/Grace-Website',
-  assetPrefix: '/Grace-Website/',
-};
+const nextConfig = {};
 
 export default nextConfig;
