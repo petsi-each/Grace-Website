8d4dca898f3f4511b76c2446ef13439d0c4556a7
segalv <anaclarasvp@usp.br>
2024-05-30 11:23:10 -0300

    Reajuste de margens negativas das imagens utilizadas no background

--- src/components/home/Oqueeograce.tsx
+++ src/components/home/Oqueeograce.tsx
@@ -44,7 +44,7 @@ export default function Oqueeograce() {
 
     return (
         <div className="my-16">
-            <img src="/home/oqueeograceBGT.svg" className="-mb-1 w-screen" /> {/*priorizar uso de w-screen over w-full*/}
+            <img src="/home/oqueeograceBGT.svg" className="-mb-2 w-screen" /> {/*priorizar uso de w-screen over w-full*/}
             <section className="bg-gradient-to-b from-vermelhoGrace to-[#feb581] py-2 px-32 flex flex-col items-center"> {/*preferi sem justify-center*/}
                 <h1 className="mb-12 font-Poppins font-bold text-brancoGrace text-4xl">O que fazemos?</h1>
                 <p className="font-Poppins text-center text-brancoGrace text-lg">O GRACE é um projeto de extensão do curso de Sistemas de Informação (Escola de Artes, CIências e Humanidades) da Universidade de São Paulo. Nosso objetivo é <b>incentivar</b> e <b>manter</b> meninas na área da computação, a fim de diminuir a disparidade de gênero, por meio de atividades práticas e tecnológicas!</p>
@@ -58,7 +58,7 @@ export default function Oqueeograce() {
                     }                    
                 </div>
             </section>
-            <img src="/home/oqueeograceBGB.svg" className="w-screen" />
+            <img src="/home/oqueeograceBGB.svg" className="-mb-2 w-screen" />
         </div>
     );
 }
