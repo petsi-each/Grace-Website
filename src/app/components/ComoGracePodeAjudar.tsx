bc81700d8d129af44eb8091f9751fa54d7504b76
Anemaygi <gianemayumisan@gmail.com>
2024-06-18 23:31:20 +0800

    style: style options di

--- src/app/components/ComoGracePodeAjudar.tsx
+++ src/app/components/ComoGracePodeAjudar.tsx
@@ -12,43 +12,43 @@ export default function ComoGracePodeAjudar() {
             title: "Professores e outros grupos",
             description: "Veja nossa galeria de atividades para replicá-las com seu público!",
             icon: <TeacherIcon />,
-            color: "#E7442B"
+            color: "bg-vermelhoGrace"
         },
         {
             title: "Escolas",
             description: "Entre em contato para organizar uma atividade com suas alunas",
             icon: <SchoolIcon />,
-            color: "#FA8632"
+            color: "bg-laranjaGrace"
         },
         {
             title: "Estudantes de graduação",
             description: "Vire uma amiga do GRACE, junte-se à nossa comunidade e nos ajude a organizar atividades!",
             icon: <AssignmentIcon />,
-            color: "#FFB884"
+            color: "bg-pessegoGrace"
         },
         {
             title: "Empresas",
             description: "Entre em contato para organizarmos uma atividade para as alunas de graduação da USP!",
             icon: <ApartmentIcon />,
-            color: "#E7442B"
+            color: "bg-vermelhoGrace"
         },
     ]
 
     return (
-        <section className="grid grid-cols-2 gap-8">
+        <section className="grid grid-cols-2 gap-8 font-Poppins text-cinzaGrace">
             <div className="bg-green-500">
                 {/* <img src="./home/grace-ajuda-1.png" alt="Membra do GRACE fazendo high five com uma criança" /> */}
                 {/* <img src="./home/grace-ajuda-2.png" alt="Na frente, membra do GRACE monitorando duas alunas em atividade. Atrás, outra membra auxiliando outras alunas" /> */}
             </div>
             <div>
-                <h2 className="font-Poppins text-4xl font-bold text-cinzaGrace">Como o <span className="text-vermelhoGrace">GRACE</span> pode me ajudar?</h2>
+                <h2 className="text-4xl font-bold ">Como o <span className="text-vermelhoGrace">GRACE</span> pode me ajudar?</h2>
 
                 {
                     options.map((option, idx) => (
-                        <div>
-                            <span>{option.icon}</span>
+                        <div className="flex items-center mb-4 space-x-4" key={idx}>
+                            <span className={`${option.color} drop-shadow-lg rounded-full h-20 w-20 flex items-center justify-center`}>{option.icon}</span>
                             <span>
-                                <h3>{option.title}</h3>
+                                <h3 className="font-bold text-2xl">{option.title}</h3>
                                 <p>{option.description}</p>
                             </span>
                         </div>
