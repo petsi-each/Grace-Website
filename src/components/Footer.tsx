c2f55b1d9e9a0cb48a30d0bcb409e2adc9b5fd72
Fernando Toba Lopez <fetobalopez@gmail.com>
2024-07-03 13:52:09 -0300

    fix: Implement mt-10 and h-full for footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
 export default function Footer() {
   return (
-    <footer className="bg-transparent position:relative flex flex-col justify-center items-center w-full before:content-[''] before:absolute before:w-full before:h-[14.7rem] before:bg-[url('/waves.svg')] before:bg-no-repeat before:bg-top before:-z-10">
+    <footer className="bg-transparent relative flex flex-col justify-center items-center w-full h-full mt-14 before:content-[''] before:absolute before:w-full before:h-[14.7rem] before:bg-[url('/waves.svg')] before:bg-no-repeat before:bg-top before:-z-10">
+      <div className=" "> </div >
       
 
 
