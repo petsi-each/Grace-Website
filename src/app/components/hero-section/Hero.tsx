76881822d837ac20e7d76e0bce84da12559a6b97
thais-fujissaka <80991615+thais-fujissaka@users.noreply.github.com>
2024-07-05 18:13:23 -0300

    adiciona imagens do background

--- src/app/components/hero-section/Hero.tsx
+++ src/app/components/hero-section/Hero.tsx
@@ -10,8 +10,9 @@ import patternXLaranja from "/images/pattern-x-laranja.png";*/
 
 export default function Hero(){
     return (
-        <section className="flex place-content-center h-dvg">
-
+        <section className="flex place-content-center relative">
+            
+            {/* 2 colunas*/}
             <div className="grid grid-cols-8 gap-0 space-x-8 py-10">
 
                 <div className="col-span-5 content-center">
@@ -26,8 +27,7 @@ export default function Hero(){
                         </h1>
 
                         {/* Descrição GRACE */}
-
-                        <p className="pt-5 pb-9 font-medium leading-5">O GRACE tem como objetivo impulsionar a participação feminina na Computação e no Empreendedorismo desde o Ensino Fundamental até a Graduação.</p>
+                         <p className="pt-5 pb-9 font-medium leading-5">O GRACE tem como objetivo impulsionar a participação feminina na Computação e no Empreendedorismo desde o Ensino Fundamental até a Graduação.</p>
 
                     </div>
 
@@ -42,26 +42,29 @@ export default function Hero(){
                             <SecondaryButton onClick={() => window.open("https://www.instagram.com/graceusp/", "_blank")} title={"Galeria de Atividades ->"} />
                         </span>
 
-                        <div>
-                        <ImageG src="/home/pattern-x-cinza.png" height={0} width={0} alt="Imagem decorativa de fundo"/>
-                        </div>
                     </span>
+
+                    
 
                 </div>
 
                 {/* Imagem da mayu */}
-                <div className="inline col-span-3 max-w-fit static">
-                    <ImageG src="/home/hero-image.png" height={0} width={0} alt="Menina sorrindo testando um circuito de Arduino"/>
-
-                    <div className="absolute">
-                        <ImageG className="w-10 h-10" src="/home/pattern-x-branco.png" height={110} width={110} alt="Imagem decorativa de fundo"/>
-                    </div>
-
-                    <div className="absolute">
-                        <ImageG src="/home/pattern-x-laranja.png" height={0} width={0} alt="Imagem decorativa de fundo"/>
-                    </div>
+                <div className="inline col-span-3 w-full static">
+                    <ImageG className="w-96" src="/home/mayu-hero.png" height={0} width={0} alt="Menina sorrindo testando um circuito de Arduino"/>
                 </div>
                 
+            </div>
+
+            <div className="absolute top-40 left-0 -z-40">
+                <ImageG className="w-36" src="/home/pattern-x-cinza.png" height={0} width={0} alt="Imagem decorativa de fundo"/>
+            </div>
+
+            <div className="absolute bottom-0 right-0">
+                <ImageG className="w-72" src="/home/pattern-x-branco.png" height={110} width={110} alt="Imagem decorativa de fundo"/>
+            </div>
+
+            <div className="absolute -top-20 right-1 -z-40">
+                <ImageG className="w-52" src="/home/pattern-x-laranja.png" height={0} width={0} alt="Imagem decorativa de fundo"/>
             </div>
             
         </section>
