4a0030473bb028d1f7acb74ac9b90eb30a60df0a
segalv <anaclarasvp@usp.br>
2024-07-05 13:32:37 -0300

    Reajuste de breaking points

--- src/app/components/BalaoGrid.tsx
+++ src/app/components/BalaoGrid.tsx
@@ -13,13 +13,13 @@ interface BalaoList {
 
 export default function BalaoGrid(props: BalaoList) {
     return (
-        <div className="mt-8 md:mt-12 lg:mt-16 grid grid-cols-2 md:gri-cols-3 gap-x-3 md:gap-x-5 gap-y-2 md:gap-y-4"> {/*número de colunas não está sendo corretamente definido para os diferentes tamanhos de tela --> todos estão com duas colunas*/}
+        <div className="mt-8 md:mt-12 lg:mt-16 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4 lg:gap-x-5 gap-y-3 lg:gap-y-4"> 
             {
                 props.lista.map((balao) => (
                     <div>
-                        <article className="flex flex-col p-2 md:p-4 lg:p-6 w-28 md:w-52 lg:w-72 h-28 md:h-52 lg:h-72 bg-brancoGrace rounded-3xl shadow-md"> {/*rever o tamanho dos balões!*/}{/*definir breaking points para as tags inferiores:*/}
-                            <ImageG className="w-60 h-40 rounded-3xl" src={balao.imagem} alt={balao.alternativo} width="2" height="2"/>
-                            <h2 className="font-Poppins font-bold text-vermelhoGrace text-lg flex justify-center m-2">{balao.titulo}</h2>
+                        <article className="flex flex-col p-4 xl:p-6 w-60 xl:w-72 h-60 xl:h-72 bg-brancoGrace rounded-2xl xl:rounded-3xl shadow-md"> {/*breaking points extras para telas xl*/}
+                            <ImageG className="w-52 xl:w-60 rounded-2xl xl:rounded-3xl flex justify-center" src={balao.imagem} alt={balao.alternativo} width="2" height="2"/>
+                            <h2 className="font-Poppins font-bold text-vermelhoGrace text-lg flex justify-center my-0.5 xl:my-2">{balao.titulo}</h2>
                             <p className="font-Poppins text-center text-black text-xs">{balao.descricao}</p>
                         </article>
                     </div>
