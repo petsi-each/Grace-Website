422ab9fdb04b4fb77798873dded517c6306086e4
Anemaygi <gianemayumisan@gmail.com>
2024-07-06 13:57:24 +0800

    fix: add key to loop

--- src/app/components/BalaoGrid.tsx
+++ src/app/components/BalaoGrid.tsx
@@ -15,8 +15,8 @@ export default function BalaoGrid(props: BalaoList) {
     return (
         <div className="mt-8 md:mt-12 lg:mt-16 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4 lg:gap-x-5 gap-y-3 lg:gap-y-4"> 
             {
-                props.lista.map((balao) => (
-                    <div>
+                props.lista.map((balao,idx) => (
+                    <div key={idx}>
                         <article className="flex flex-col p-4 xl:p-6 w-60 xl:w-72 h-60 xl:h-72 bg-brancoGrace rounded-2xl xl:rounded-3xl shadow-md"> {/*breaking points extras para telas xl*/}
                             <ImageG className="w-52 xl:w-60 rounded-2xl xl:rounded-3xl flex justify-center" src={balao.imagem} alt={balao.alternativo} width="2" height="2"/>
                             <h2 className="font-Poppins font-bold text-vermelhoGrace text-lg flex justify-center my-0.5 xl:my-2">{balao.titulo}</h2>
